2dfdeccbf62c32c51f8044d94a522ea9cdc72c57
Victor Li <victorli5674@yahoo.com>
2017-08-17 14:33:39 -0700

    refactored passport logins by not using the db promise but the db instance itself

--- config/passport.js
+++ config/passport.js
@@ -5,14 +5,14 @@ var crypto = require('crypto');
 
 var dbUsers = require('../models/users');
 var secret = require('../secret');
-var dbConnection = require('../models/dbConnection');
 
 passport.serializeUser(function (user, cb) {
     cb(null, user.id);
 });
 
-passport.deserializeUser(function (id, cb) {
-    dbUsers.findById(dbConnection, id, function (err, user) {
+passport.deserializeUser(function (req, id, cb) {
+    var db = req.app.get('db');
+    dbUsers.findById(db, id, function (err, user) {
         if(err) { return cb(err); }
         cb(null, user);
     });
@@ -21,7 +21,8 @@ passport.deserializeUser(function (id, cb) {
 passport.use('local-login', new localStrategy({
         passReqToCallback: true
     }, function (req, username, password, cb) {
-        dbUsers.findByUsername(dbConnection, username, function (err, userObj) {
+        var db = req.app.get('db');
+        dbUsers.findByUsername(db, username, function (err, userObj) {
             if(err) { return cb(err); }
             if(!userObj) { return cb(null, false, { message: 'Wrong credentials' }); }
             if(userObj.password != password) { return cb(null, false, { message: 'Wrong credentials' }); }
@@ -33,7 +34,8 @@ passport.use('local-login', new localStrategy({
 passport.use('local-register', new localStrategy({
         passReqToCallback: true
     }, function (req, username, password, cb) {
-        dbUsers.registerByUsername(dbConnection, username, password, req.body.displayName, function (err, userObj) {
+        var db = req.app.get('db');
+        dbUsers.registerByUsername(db, username, password, req.body.displayName, function (err, userObj) {
             if(err) { return cb(err); }
             if(!userObj) { return cb(null, false, { message: 'Username ' + username + ' has already been taken'}); }
             return cb(null, userObj);
@@ -45,8 +47,9 @@ passport.use(new facebookStrategy({
     clientID: secret.FB_CLIENT_ID,
     clientSecret: secret.FB_CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/login/facebook/return',
-    profileFields: ['id', 'displayName', 'email']
-}, function (accessToken, refreshToken, profile, cb) {
+    profileFields: ['id', 'displayName', 'email'],
+    passReqToCallback: true
+}, function (req, accessToken, refreshToken, profile, cb) {
 
     var id = profile.id;
     var displayName = profile.displayName;
@@ -54,7 +57,8 @@ passport.use(new facebookStrategy({
     var usernameHash = crypto.createHash('md5').update(id).digest('hex');
     var passwordHash = crypto.createHash('md5').update(id + displayName).digest('hex');
 
-    dbUsers.fbUser(dbConnection, usernameHash, passwordHash, displayName, function (err, userObj) {
+    var db = req.app.get('db');
+    dbUsers.fbUser(db, usernameHash, passwordHash, displayName, function (err, userObj) {
         if(err) { return cb(err); }
         return cb(null, userObj);
     });
--- models/users.js
+++ models/users.js
@@ -1,76 +1,68 @@
 module.exports = {
 
-    findById: function (dbConnection, id, cb) {
-        dbConnection.then((db) => {
-            db.users.findOne({
-                id: id
-            }).then((user) => {
-                if(user) cb(null, user);
-                else { cb(new Error('User ' + id + ' does not exist.')); }
-            });
+    findById: function (db, id, cb) {
+        db.users.findOne({
+            id: id
+        }).then((user) => {
+            if(user) cb(null, user);
+            else { cb(new Error('User ' + id + ' does not exist.')); }
         });
     },
 
-    findByUsername: function (dbConnection, username, cb) {
-        dbConnection.then((db) => {
-            db.users.findOne({
-                username: username
-            }).then((user) => {
-                if(user) cb(null, user);
-                else { cb(null, null); }
-            });
+    findByUsername: function (db, username, cb) {
+        db.users.findOne({
+            username: username
+        }).then((user) => {
+            if(user) cb(null, user);
+            else { cb(null, null); }
         });
 
     },
 
-    registerByUsername: function (dbConnection, username, password, displayname, cb) {
-        dbConnection.then((db) => {
-            db.users.findOne({
-                username: username
-            }).then((user) => {
+    registerByUsername: function (db, username, password, displayname, cb) {
+        db.users.findOne({
+            username: username
+        }).then((user) => {
 
-                //adds a new user, since db cannot find one
-                if(!user) {
+            //adds a new user, since db cannot find one
+            if(!user) {
 
-                    db.users.insert({
-                        displayname: displayname,
-                        username: username,
-                        password: password
-                    }).then((newUser) => {
-                        if(newUser) cb(null, newUser);
-                        else { cb(new Error('Something wrong with inserting with registerByUsername function')); }
-                    });
+                db.users.insert({
+                    displayname: displayname,
+                    username: username,
+                    password: password
+                }).then((newUser) => {
+                    if(newUser) cb(null, newUser);
+                    else { cb(new Error('Something wrong with inserting with registerByUsername function')); }
+                });
 
-                } else {
-                    cb(null, null);
-                }
+            } else {
+                cb(null, null);
+            }
 
-            });
         });
 
     },
 
-    fbUser: function (dbConnection, username, password, displayName, cb) {
-        dbConnection.then((db) => {
-            db.users.findOne({
-                username: username
-            }).then((user) => {
+    fbUser: function (db, username, password, displayName, cb) {
+        db.users.findOne({
+            username: username
+        }).then((user) => {
 
-                if(user) cb(null, user);
-                else {
+            if(user) cb(null, user);
+            else {
 
-                    db.users.insert({
-                        displayname: displayName,
-                        username: username,
-                        password: password
-                    }).then((newUser) => {
-                        if(newUser) cb(null, newUser);
-                        else { cb(new Error('Something wrong with inserting with fbUser function')); }
-                    });
+                db.users.insert({
+                    displayname: displayName,
+                    username: username,
+                    password: password
+                }).then((newUser) => {
+                    if(newUser) cb(null, newUser);
+                    else { cb(new Error('Something wrong with inserting with fbUser function')); }
+                });
 
-                }
+            }
 
-            });
         });
 
     }
