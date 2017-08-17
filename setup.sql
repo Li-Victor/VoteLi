e701fb0c262618f0b7dfa897ffe10e5431ba7a58
Victor Li <victorli5674@yahoo.com>
2017-08-17 10:45:52 -0700

    scripts for the rest of the tables

--- setup.sql
+++ setup.sql
@@ -1,6 +1,26 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     displayName TEXT NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL
+);
+
+CREATE TABLE poll (
+    pollid SERIAL NOT NULL PRIMARY KEY,
+    userid SERIAL NOT NULL,
+    question TEXT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE log (
+    pollid SERIAL NOT NULL,
+    userip TEXT NOT NULL,
+    FOREIGN KEY (pollid) REFERENCES poll(pollid) ON DELETE CASCADE
+);
+
+CREATE TABLE choices (
+    pollid SERIAL NOT NULL,
+    option TEXT NOT NULL,
+    votes INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (pollid) REFERENCES poll(pollid) ON DELETE CASCADE
 );
