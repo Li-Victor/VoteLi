ef57d32dda54ffc1b84c802c7b5bdb305938b9ba
Victor Li <victorli5674@yahoo.com>
2017-10-22 21:56:07 -0700

    valitdate and sanitize new poll page form

--- client/src/components/NewPollPage.js
+++ client/src/components/NewPollPage.js
@@ -1,23 +1,53 @@
 import React from 'react';
+import Validator from 'validator';
+
+import isEmptyObject from '../utils/isEmptyObject';
 
 class NewPollPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      topic: '',
-      options: ''
+      data: {
+        topic: '',
+        options: ''
+      },
+      loading: false,
+      errors: {}
     };
   }
 
   onChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      data: { ...this.state.data, [e.target.name]: e.target.value }
     });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
+    const sanitizedData = {
+      topic: Validator.escape(this.state.data.topic),
+      options: Validator.escape(this.state.data.options)
+    };
+    const errors = this.validate(sanitizedData);
+    console.log(errors);
     console.log('submit form');
+  };
+
+  validate = (data) => {
+    const errors = {};
+    if (!Validator.isEmpty(data.topic)) errors.topic = 'Your poll needs a title';
+
+    const allOptions = data.options.split('\n');
+    if (allOptions.length < 2) errors.options = 'You need 2 or more options to make a poll!';
+
+    const allNoneEmptyOptions = allOptions.every(element => !Validator.isEmpty(element));
+    if (!allNoneEmptyOptions && Object.prototype.hasOwnProperty.call(errors, 'options')) {
+      errors.options += ' All options cannot be blank';
+    }
+    if (!allNoneEmptyOptions && !Object.prototype.hasOwnProperty.call(errors, 'options')) {
+      errors.options = 'All options cannot be blank';
+    }
+    return errors;
   };
 
   render() {
