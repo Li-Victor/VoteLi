dc2097082b5bd44a20c20daeca723da46f6991da
Victor Li <victorli5674@yahoo.com>
2017-10-22 18:20:56 -0700

    created simple form for new poll page

--- client/src/components/NewPollPage.js
+++ client/src/components/NewPollPage.js
@@ -1,5 +1,59 @@
 import React from 'react';
 
-const NewPollPage = () => <p>New Poll</p>;
+class NewPollPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: '',
+      options: ''
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit form');
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Make a new poll!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="topic">
+            Topic:
+            <input
+              type="text"
+              id="topic"
+              name="topic"
+              value={this.state.topic}
+              onChange={this.onChange}
+            />
+          </label>
+
+          <br />
+
+          <label htmlFor="options">
+            Options (seperated by line):
+            <textarea
+              id="options"
+              name="options"
+              value={this.state.options}
+              onChange={this.onChange}
+            />
+          </label>
+
+          <br />
+          <input type="submit" value="Make!" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default NewPollPage;
