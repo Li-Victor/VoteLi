ea793d53a6e7c6b152212d403ff07e1706aab28c
Victor Li <victorli5674@yahoo.com>
2017-10-26 21:24:52 -0700

    displaying user's polls in MyPollsPage

--- client/src/components/MyPollsPage.js
+++ client/src/components/MyPollsPage.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const MyPollsPage = () => <p>My Polls</p>;
+const MyPollsPage = ({ polls }) => {
+  const listOfPolls = polls.map(poll => <p key={poll.pollid}>{poll.topic}</p>);
 
-export default MyPollsPage;
+  return (
+    <div>
+      <p>My Polls</p>
+      <div>{listOfPolls}</div>
+    </div>
+  );
+};
+
+MyPollsPage.propTypes = {
+  polls: PropTypes.arrayOf(
+    PropTypes.shape({
+      pollid: PropTypes.number,
+      topic: PropTypes.string,
+      userid: PropTypes.string
+    })
+  ).isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    polls: state.user.polls
+  };
+}
+
+export default connect(mapStateToProps)(MyPollsPage);
