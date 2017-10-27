c93a0b10214613467b4e6af8f9f00e3ced84395d
Victor Li <victorli5674@yahoo.com>
2017-10-26 20:52:34 -0700

    sends the polls for the current user when they are logged in

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,10 +5,16 @@ const router = express.Router();
 router.get('/current_user', (req, res) => {
   if (req.user) {
     const { id, displayname } = req.user;
-    return res.send({
-      id,
-      displayname
-    });
+    const db = req.app.get('db');
+    return db.poll
+      .find({
+        userid: id
+      })
+      .then(polls => res.send({
+        id,
+        displayname,
+        polls
+      }));
   }
   return res.send({});
 });
