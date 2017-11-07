5ad8c81f2a690d93b85ecbbe909b99cf92f4203a
Victor Li <victorli5674@yahoo.com>
2017-11-06 19:46:30 -0800

    topnav with semantic ui

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Button, Container, Icon, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import isEmptyObject from '../utils/isEmptyObject';
 
 const TopNav = ({ user }) => (
   <div>
-    <Link to="/">
-      <p>VoteLi</p>
-    </Link>
-    {isEmptyObject(user) ? (
-      <a href="/login/twitter">Log In with Twitter</a>
-    ) : (
-      <div>
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/mypolls">My Polls</Link>
-        <br />
-        <Link to="/newpoll">New Poll</Link>
-        <br />
-        <p>{user.displayname}</p>
-        <a href="/auth/logout">Logout</a>
-      </div>
-    )}
+    <Menu>
+      <Container>
+        <Menu.Item header href="/">
+          VoteLi
+        </Menu.Item>
+        {isEmptyObject(user) ? (
+          <Menu.Item>
+            <Button color="twitter" onClick={() => window.location.replace('/login/twitter')}>
+              <Icon name="twitter" /> Log in with Twitter
+            </Button>
+          </Menu.Item>
+        ) : (
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Link to="/mypolls">My Polls</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/newpoll">New Poll</Link>
+            </Menu.Item>
+            <Menu.Item>{user.displayname}</Menu.Item>
+            <Menu.Item>
+              <a href="/auth/logout">Logout</a>
+            </Menu.Item>
+          </Menu.Menu>
+        )}
+      </Container>
+    </Menu>
   </div>
 );
 
