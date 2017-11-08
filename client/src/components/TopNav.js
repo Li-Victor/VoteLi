128763865d39cdbf9576b39d3f72c5efb9198f39
Victor Li <victorli5674@yahoo.com>
2017-11-07 22:26:21 -0800

    fixed styling the topnav

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,46 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Button, Container, Icon, Menu } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
+import { Button, Container, Dropdown, Icon, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import isEmptyObject from '../utils/isEmptyObject';
 
-const TopNav = ({ user }) => (
-  <div>
-    <Menu fixed="top">
-      <Container>
-        <Menu.Item header href="/">
-          VoteLi
-        </Menu.Item>
-        {isEmptyObject(user) ? (
-          <Menu.Item>
-            <Button color="twitter" onClick={() => window.location.replace('/login/twitter')}>
-              <Icon name="twitter" /> Log in with Twitter
-            </Button>
+const TopNav = ({ user, history }) => {
+  const handleItemClick = (e, { name }) => history.push(`/${name}`);
+  const logout = () => {
+    window.location.href = '/auth/logout';
+  };
+
+  return (
+    <div>
+      <Menu fixed="top">
+        <Container>
+          <Menu.Item header href="/">
+            VoteLi
           </Menu.Item>
-        ) : (
-          <Menu.Menu position="right">
+          {isEmptyObject(user) ? (
             <Menu.Item>
-              <Link to="/mypolls">My Polls</Link>
+              <Button color="twitter" onClick={() => window.location.replace('/login/twitter')}>
+                <Icon name="twitter" /> Log in with Twitter
+              </Button>
             </Menu.Item>
-            <Menu.Item>
-              <Link to="/newpoll">New Poll</Link>
-            </Menu.Item>
-            <Menu.Item>{user.displayname}</Menu.Item>
-            <Menu.Item>
-              <a href="/auth/logout">Logout</a>
-            </Menu.Item>
-          </Menu.Menu>
-        )}
-      </Container>
-    </Menu>
-  </div>
-);
+          ) : (
+            <Menu.Menu position="right">
+              <Menu.Item name="mypolls" link onClick={handleItemClick}>
+                My Polls
+              </Menu.Item>
+              <Menu.Item name="newpoll" link onClick={handleItemClick}>
+                New Poll
+              </Menu.Item>
+              <Dropdown text={user.displayname} pointing className="link item">
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
+          )}
+        </Container>
+      </Menu>
+    </div>
+  );
+};
 
 TopNav.propTypes = {
   user: PropTypes.shape({
     displayname: PropTypes.string
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
   }).isRequired
 };
 
@@ -50,4 +61,6 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(TopNav);
+// <Menu.Item>{user.displayname}</Menu.Item>
+// <Menu.Item href="/auth/logout">Logout</Menu.Item>
+export default withRouter(connect(mapStateToProps)(TopNav));
